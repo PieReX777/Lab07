bc5c7c70810dca9fec84ff26b8af281200768683
Jeanpiero Zea <jeanpiero.zea@tecsup.edu.pe>
2024-09-16 14:14:38 -0500

    Primer comit de Lab07

---
+++ app/src/main/java/com/example/datossinmvvm/MainActivity.kt
@@ -0,0 +1,74 @@
+package com.example.datossinmvvm
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.datossinmvvm.ui.theme.DatosSinMVVMTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DatosSinMVVMTheme {
+                val navController = rememberNavController()
+                SetupNavGraph(navController = navController)
+            }
+        }
+    }
+}
+
+@Composable
+fun SetupNavGraph(navController: NavHostController) {
+    NavHost(navController = navController, startDestination = "main") {
+        composable("main") {
+            MainScreen(navController = navController)
+        }
+        composable("screen_user") {
+            ScreenUser()
+        }
+    }
+}
+
+@Composable
+fun MainScreen(navController: NavHostController) {
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+        Greeting(
+            name = "Android",
+            modifier = Modifier.padding(innerPadding)
+        )
+        Button(
+            onClick = { navController.navigate("screen_user") },
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            Text(text = "Go to Screen User")
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    DatosSinMVVMTheme {
+        Greeting("Android")
+    }
+}
